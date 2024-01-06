473250b4cdff37e090665f01f3c1dd14f4482eca
Fabrício ADM <fabriciobaraunadev@gmail.com>
2024-01-06 13:09:58 -0300

    first commit

---
+++ lg-tv-oled/index.js
@@ -0,0 +1,13 @@
+document.addEventListener('DOMContentLoaded', function() {
+    // Adiciona um evento de clique à imagem container-0
+    document.getElementById('container-0').addEventListener('click', function() {
+        // Redireciona para a outra página desejada
+        window.location.href = 'https://www.bemol.com.br/nossas-lojas';
+    });
+
+    // Adiciona um evento de clique à imagem qr-code
+    document.getElementById('qr-code').addEventListener('click', function() {
+        // Redireciona para a outra página desejada
+        window.location.href = 'https://www.bemol.com.br/nossas-lojas';
+    });
+});
